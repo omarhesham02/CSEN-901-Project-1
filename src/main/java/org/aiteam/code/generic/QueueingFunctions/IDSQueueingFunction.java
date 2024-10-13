7bc9cf9e81cb559f3bf154fc54f9a22e7e8d9894
Hosain Ghoraba <hussain.ghoraba@gmail.com>
2024-10-13 20:50:19 +0300

    change Expand() to take List<Operators> instead of Set<Operator> , for better search tree visualization

--- src/main/java/org/aiteam/code/generic/QueueingFunctions/IDSQueueingFunction.java
+++ src/main/java/org/aiteam/code/generic/QueueingFunctions/IDSQueueingFunction.java
@@ -1,6 +1,7 @@
 // src/main/java/org/aiteam/code/generic/QueueingFunctions/IDSQueueingFunction.java
 package org.aiteam.code.generic.QueueingFunctions;
 
+import java.util.List;
 import java.util.Queue;
 import java.util.Set;
 import org.aiteam.code.generic.Node;
@@ -8,7 +9,7 @@ import org.aiteam.code.generic.Node;
 public class IDSQueueingFunction implements DepthQueueingFunction {
 
     @Override
-    public Queue<Node> apply(Queue<Node> queue, Set<Node> nodes, int depth) {
+    public Queue<Node> apply(Queue<Node> queue, List<Node> nodes, int depth) {
         for (Node node : nodes) {
             if (node.getDepth() <= depth) {
                 queue.add(node);
