c33886af725b31173fad44cfed900a5e3033a99b
Omar Hesham <106028678+omarhesham02@users.noreply.github.com>
2024-10-13 12:27:47 +0300

    Implement some queueing functions

--- src/main/java/org/aiteam/code/generic/QueueingFunctions/DFSQueueingFunction.java
+++ src/main/java/org/aiteam/code/generic/QueueingFunctions/DFSQueueingFunction.java
@@ -1,25 +1,19 @@
 package org.aiteam.code.generic.QueueingFunctions;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 import org.aiteam.code.generic.Node;
 
 public class DFSQueueingFunction implements QueueingFunction {
 
-    // @Override
-    // public Queue<Node<T, V>> apply(Node<T, V> initialNode) {
-    // PriorityQueue<Node<T, V>> priorityQueue = new PriorityQueue<>(
-    // (node1, node2) -> Integer.compare(node2.depth(), node1.depth()));
-    // priorityQueue.add(initialNode);
-    // return priorityQueue;
-    // }
     @Override
     public Queue<Node> apply(Queue<Node> queue, List<Node> nodes) {
-        // Add the node to the front of the queue (DFS behavior)
-//        LinkedList<Node> linkedList = (LinkedList<Node>) queue;
-//        linkedList.addFirst(node);
         queue.addAll(nodes);
-        return queue;
+        PriorityQueue<Node> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(Node::getDepth).reversed());
+        priorityQueue.addAll(queue);
+        return priorityQueue;
     }
 }
--- src/main/java/org/aiteam/code/generic/QueueingFunctions/IDSQueueingFunction.java
+++ src/main/java/org/aiteam/code/generic/QueueingFunctions/IDSQueueingFunction.java
@@ -1,17 +1,19 @@
+// src/main/java/org/aiteam/code/generic/QueueingFunctions/IDSQueueingFunction.java
 package org.aiteam.code.generic.QueueingFunctions;
 
 import java.util.List;
 import java.util.Queue;
-
 import org.aiteam.code.generic.Node;
 
-public class IDSQueueingFunction implements QueueingFunction {
+public class IDSQueueingFunction implements DepthQueueingFunction {
 
     @Override
-    public Queue<Node> apply(Queue<Node> queue, List<Node> nodes) {
-        // Implement the IDS specific logic here
-        // For now, let's just add the node to the queue and return it
-        queue.addAll(nodes);
+    public Queue<Node> apply(Queue<Node> queue, List<Node> nodes, int depth) {
+        for (Node node : nodes) {
+            if (node.getDepth() <= depth) {
+                queue.add(node);
+            }
+        }
         return queue;
     }
 }
