0f80df3c1765ff840294b5f362ff319d51745a55
Hosain Ghoraba <hussain.ghoraba@gmail.com>
2024-10-13 13:42:37 +0300

    modify equality check of 2 states

--- src/main/java/org/aiteam/code/watersort/WaterSortState.java
+++ src/main/java/org/aiteam/code/watersort/WaterSortState.java
@@ -19,20 +19,12 @@ public class WaterSortState extends SearchState {
     @Override
     public boolean equals(Object obj) {
         // Two bottles are equal if they have the same layers
-        if (obj instanceof WaterSortState other) {
-            Bottle[] bottles = (Bottle[]) getValue();
-            Bottle[] otherBottles = (Bottle[]) other.getValue();
-            if (bottles.length != otherBottles.length) {
-                return false;
-            }
-            for (int i = 0; i < bottles.length; i++) {
-                if (!bottles[i].equals(otherBottles[i])) {
-                    return false;
-                }
-            }
-            return true;
-        }
-        return false;
+        if (!(obj instanceof WaterSortState other))
+            return false;
+
+        Bottle[] bottles = (Bottle[]) getValue();
+        Bottle[] otherBottles = (Bottle[]) other.getValue();
+        return bottles.equals(otherBottles);
     }
 
     @Override
