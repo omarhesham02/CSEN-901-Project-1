0b11a32598101f8e19605add2f82634ffc417a93
Omar Hesham <106028678+omarhesham02@users.noreply.github.com>
2024-10-13 12:27:47 +0300

    Implement some queueing functions

--- src/main/java/org/aiteam/code/generic/QueueingFunctions/BFSQueueingFunction.java
+++ src/main/java/org/aiteam/code/generic/QueueingFunctions/BFSQueueingFunction.java
@@ -1,6 +1,8 @@
 package org.aiteam.code.generic.QueueingFunctions;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 import org.aiteam.code.generic.Node;
@@ -9,8 +11,9 @@ public class BFSQueueingFunction implements QueueingFunction {
 
     @Override
     public Queue<Node> apply(Queue<Node> queue, List<Node> nodes) {
-        // Add the node to the end of the queue (BFS behavior)
         queue.addAll(nodes);
-        return queue;
+        PriorityQueue<Node> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(Node::getDepth));
+        priorityQueue.addAll(queue);
+        return priorityQueue;
     }
 }
--- src/main/java/org/aiteam/code/generic/QueueingFunctions/UCSQueueingFunction.java
+++ src/main/java/org/aiteam/code/generic/QueueingFunctions/UCSQueueingFunction.java
@@ -1,6 +1,8 @@
 package org.aiteam.code.generic.QueueingFunctions;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 import org.aiteam.code.generic.Node;
@@ -9,9 +11,9 @@ public class UCSQueueingFunction implements QueueingFunction {
 
     @Override
     public Queue<Node> apply(Queue<Node> queue, List<Node> nodes) {
-        // Implement the UCS specific logic here
-        // For now, let's just add the node to the queue and return it
         queue.addAll(nodes);
-        return queue;
+        PriorityQueue<Node> priorityQueue = new PriorityQueue<>(Comparator.comparingDouble(Node::getPathCost));
+        priorityQueue.addAll(queue);
+        return priorityQueue;
     }
 }
