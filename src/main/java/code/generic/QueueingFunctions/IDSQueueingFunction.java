b6d477dc0c38f9936478e6ec297950a64dc45a96
Omar Hesham <106028678+omarhesham02@users.noreply.github.com>
2024-10-14 16:34:16 +0300

    Fix queueing functions

--- src/main/java/code/generic/QueueingFunctions/IDSQueueingFunction.java
+++ src/main/java/code/generic/QueueingFunctions/IDSQueueingFunction.java
@@ -1,21 +1,26 @@
 // src/main/java/org/aiteam/code/generic/QueueingFunctions/IDSQueueingFunction.java
 package code.generic.QueueingFunctions;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Queue;
-import java.util.Set;
-
 import code.Node;
 
-public class IDSQueueingFunction implements DepthQueueingFunction {
+public class IDSQueueingFunction implements QueueingFunction {
+    private int depth = 1;
 
     @Override
-    public Queue<Node> apply(Queue<Node> queue, List<Node> nodes, int depth) {
+    public Queue<Node> apply(Queue<Node> queue, List<Node> nodes) {
         for (Node node : nodes) {
             if (node.getDepth() <= depth) {
                 queue.add(node);
             }
         }
-        return queue;
+        PriorityQueue<Node> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(Node::getDepth).reversed());
+        priorityQueue.addAll(queue);
+
+        ++depth;
+        return priorityQueue;
     }
 }
