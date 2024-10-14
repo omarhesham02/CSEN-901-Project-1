d35e013c94b089dec1954aecb0d4089ca4a3dd59
Hosain Ghoraba <hussain.ghoraba@gmail.com>
2024-10-14 20:59:01 +0300

    cleaning

--- src/main/java/code/WaterSortSearch.java
+++ src/main/java/code/WaterSortSearch.java
@@ -14,7 +14,8 @@ public class WaterSortSearch extends GenericSearch {
     public static int numberOfBottles;
     public static int bottleCapacity;
 
-    public static String solve(String initialState, String strategy, boolean visualize) throws CloneNotSupportedException {
+    public static String solve(String initialState, String strategy, boolean visualize)
+            throws CloneNotSupportedException {
 
         numberOfBottles = 0;
         bottleCapacity = 0;
@@ -29,6 +30,8 @@ public class WaterSortSearch extends GenericSearch {
             return "NOSOLUTION";
 
         // TODO: Why is this done with a linked list?
+        // answer : because adding a node to the beginning of a stringBuilder is O(n)
+        // while adding to the beginning of a linkedList is O(1)
         LinkedList<String> planBuilder = new LinkedList<>();
 
         String pathCost = solutionNode.getPathCost() + "";
@@ -50,5 +53,19 @@ public class WaterSortSearch extends GenericSearch {
         result = result.substring(1, result.length() - 1);
         result = result.replace(" ", "");
         return result;
+    }
+
+    public static void main(String[] args) {
+        try {
+            String state = "3;" +
+                    "4;" +
+                    "r,y,r,y;" +
+                    "y,r,y,r;" +
+                    "e,e,e,e;";
+            String strategy = "BF";
+            System.out.println(solve(state, strategy, true));
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
     }
 }
