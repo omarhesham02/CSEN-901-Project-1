ce12014bfda2e5a9e929574f2fe4c2a7c1aaaf5e
mahmoudaboueleneen <mahmoudaboueleneen@gmail.com>
2024-10-14 22:00:23 +0300

    Add heuristic one

--- src/main/java/code/generic/QueueingFunctions/AStar1QueueingFunction.java
+++ src/main/java/code/generic/QueueingFunctions/AStar1QueueingFunction.java
@@ -2,11 +2,13 @@ package code.generic.QueueingFunctions;
 
 import java.util.Comparator;
 import java.util.PriorityQueue;
+
 import code.Node;
+import code.watersort.WaterSortProblem;
 
 public class AStar1QueueingFunction implements QueueingFunction {
     @Override
     public PriorityQueue<Node> apply() {
-        return new PriorityQueue<>(Comparator.comparingInt(node -> node.getPathCost() + node.getHeuristic1()));
+        return new PriorityQueue<>(Comparator.comparingInt(node -> node.getPathCost() + WaterSortProblem.getHeuristicOne(node)));
     }
 }
--- src/main/java/code/generic/QueueingFunctions/QueueingFunction.java
+++ src/main/java/code/generic/QueueingFunctions/QueueingFunction.java
@@ -1,6 +1,5 @@
 package code.generic.QueueingFunctions;
 
-import java.util.List;
 import java.util.PriorityQueue;
 
 import code.Node;
