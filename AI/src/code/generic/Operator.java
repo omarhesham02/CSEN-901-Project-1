cb63f31856b3bdc9a7f51d9a660fa3a93edf8302
Omar Hesham <106028678+omarhesham02@users.noreply.github.com>
2024-10-20 00:46:17 +0300

    Delete AI Folder

--- AI/src/code/generic/Operator.java
+++
@@ -1,7 +0,0 @@
-package code.generic;
-
-public interface Operator {
-    OperatorResult apply(SearchState state) throws CloneNotSupportedException;
-
-    boolean isApplicable(SearchState state);
-}
